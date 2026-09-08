bf80a29e0982deebadefdb68bff9cfa49e7a79f6
Gypopo118 <slonopo@gmail.com>
2026-09-08 21:45:47 +0300

    fix: missing Intent import in DialerInCallService

--- android/app/src/main/java/com/gypopo/dialer/DialerInCallService.java
+++ android/app/src/main/java/com/gypopo/dialer/DialerInCallService.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.telecom.Call;
 import android.telecom.CallAudioState;
